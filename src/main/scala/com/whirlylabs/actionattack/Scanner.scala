cca3290f98797d5953299692ed94e16dfcda2259
Andrei Dreyer <andrei.dreyer1997@gmail.com>
2024-11-22 16:59:11 +0200

    Implemented plugin repo downloading from actions (#15)

--- src/main/scala/com/whirlylabs/actionattack/Scanner.scala
+++ src/main/scala/com/whirlylabs/actionattack/Scanner.scala
@@ -31,8 +31,10 @@ class Scanner(db: Database) extends Runnable, AutoCloseable {
           cloneRepo(repository, commit) match {
             case Success(repoPath) =>
               try {
-                val findings = runScan(repoPath, commit)
-                db.storeResults(commit, findings)
+                val workflowFiles = getWorkflowFiles(repoPath)
+                if !shouldScanPluginRepos(workflowFiles) then
+                  val findings = runScan(repoPath, commit, workflowFiles)
+                  db.storeResults(commit, findings)
               } catch {
                 case e: Exception =>
                   logger.error("Exception occurred while running scan & storing results", e)
@@ -49,7 +51,7 @@ class Scanner(db: Database) extends Runnable, AutoCloseable {
     }
   }
 
-  private def cloneRepo(repository: Repository, commit: Commit): Try[Path] = Try {
+  private def cloneRepo(repository: Repository, commit: Commit, fetchTags: Boolean = false): Try[Path] = Try {
     val targetDir = Files.createTempDirectory(s"action-attack-${repository.owner}-${repository.name}-")
     val repoUrl   = repository.toUrl
     val cloneCmd  = Seq("git", "clone", repoUrl.toString, targetDir.toAbsolutePath.toString, "--depth", "1")
@@ -65,7 +67,16 @@ class Scanner(db: Database) extends Runnable, AutoCloseable {
       val msg = new String(fetchProcess.getErrorStream.readAllBytes())
       throw new RuntimeException(s"Error occurred while fetching the target commit! Details: $msg")
     }
-    val checkoutCmd     = Seq("git", "checkout", commit.sha)
+    if (fetchTags) {
+      val fetchTagsCmd     = Seq("git", "fetch", "--tags")
+      val fetchProcessTags = ProcessBuilder(fetchTagsCmd*).directory(targetDir.toFile).startBlocking
+      if (fetchProcessTags.exitValue() != 0) {
+        val msg = new String(fetchProcessTags.getErrorStream.readAllBytes())
+        throw new RuntimeException(s"Error occurred while fetching the target commit! Details: $msg")
+      }
+    }
+    val checkoutCmd = Seq("git", "checkout", commit.sha)
+    logger.debug(s"Checkout cmd: ${checkoutCmd.mkString(" ")}")
     val checkoutProcess = ProcessBuilder(checkoutCmd*).directory(targetDir.toFile).startBlocking
     if (checkoutProcess.exitValue() != 0) {
       val msg = new String(checkoutProcess.getErrorStream.readAllBytes())
@@ -74,51 +85,107 @@ class Scanner(db: Database) extends Runnable, AutoCloseable {
     targetDir
   }
 
-  private def runScan(repositoryPath: Path, commit: Commit): List[Finding] = {
+  private def shouldScanPluginRepos(workflowFiles: List[(Path, GitHubActionsWorkflow)]): Boolean = {
 
+    /** Gets the names for all the plugins used on this repository
+      * @return
+      *   List of plugin names in the form of `pluginOwner/pluginName@version`
+      */
+    def fetchPluginNames: List[String] = {
+      logger.debug(s"Running check for plugins on repo")
+      // Download plugins on files
+      workflowFiles.flatMap { case (_, actionsFile) =>
+        actionsFile.jobs.values.flatMap { job =>
+          job.steps.map { step =>
+            step.uses.map(_.value.strip).getOrElse("")
+          }
+        }
+      }.distinct
+    }
+
+    val pluginNames = fetchPluginNames
+    // TODO: Check if there are any that need to be downloaded against plugins table
+    val pluginsToDownload = pluginNames.slice(0, 2)
+
+    // TODO: Move this to plugin scanner area once done. We only want to check if we need to scan plugins
+    //  here to early exit or not.
+    val targetDirs = pluginsToDownload.map { plugin =>
+      val Array(ownerRepo, actionVersion) = plugin.split("@")
+      val Array(owner, repoName)          = ownerRepo.split("/")
+
+      val repo   = Repository(id = -1, owner = owner, name = repoName)
+      val commit = Commit(sha = actionVersion, false, false, -1)
+
+      cloneRepo(repo, commit, fetchTags = true) match {
+        case Success(targetDir) =>
+          Some(targetDir)
+        case Failure(e) =>
+          logger.warn(s"Error cloning plugin repo: ${plugin} ${e}")
+          None
+      }
+    }
+
+    logger.info(s"${pluginNames.size} plugins found in repo")
+    // TODO: Uncomment once check against plugins table is implemented
+//    pluginNames.nonEmpty
+    false
+  }
+
+  private def getWorkflowFiles(repositoryPath: Path): List[(Path, GitHubActionsWorkflow)] = {
     def isYAMLFile(path: Path): Boolean = {
       val fileName = path.getFileName.toString
       fileName.endsWith(".yml") || fileName.endsWith(".yaml")
     }
 
+    val githubPath = repositoryPath.resolve(".github").resolve("workflows")
+    if (Files.exists(githubPath) && Files.isDirectory(githubPath)) {
+      // Run through GitHub directory for YAML files
+      val candidates = Files
+        .walk(githubPath)
+        .iterator()
+        .asScala
+        .filter(path => Files.isRegularFile(path) && isYAMLFile(path))
+        .toList
+      // Parse all yaml files and keep those that are workflow files
+      val a = candidates
+        .flatMap { path =>
+          yamlToGHWorkflow(Files.readString(path)) match {
+            case Success(workflow) =>
+              Option(path -> workflow)
+            case Failure(_: ScannerException) =>
+              logger.warn(s"Unable to parse '$path', it appears to be an invalid YAML file")
+              None
+            case Failure(e) if !path.getParent.equals(githubPath) =>
+              logger.debug(
+                s"Unable to parse '$path' as a GitHub workflow file, however it's likely that it's not one",
+                e
+              )
+              None
+            case Failure(e) =>
+              logger.warn(s"Unable to parse '$path' as a GitHub workflow file", e)
+              None
+          }
+        }
+      a
+    } else {
+      List.empty
+    }
+  }
+
+  private def runScan(
+    repositoryPath: Path,
+    commit: Commit,
+    workflowFiles: List[(Path, GitHubActionsWorkflow)]
+  ): List[Finding] = {
     def runInternal: List[Finding] = {
       // TODO: Scanners to run should be configurable
       lazy val scannersToRun = CommandInjectionScanner() :: Nil
 
       val githubPath = repositoryPath.resolve(".github").resolve("workflows")
       if (Files.exists(githubPath) && Files.isDirectory(githubPath)) {
-        // Run through GitHub directory for YAML files
-        val candidates = Files
-          .walk(githubPath)
-          .iterator()
-          .asScala
-          .filter(path => Files.isRegularFile(path) && isYAMLFile(path))
-          .toList
-        // Parse all yaml files and keep those that are workflow files
-        val workflowFiles = candidates
-          .flatMap { path =>
-            yamlToGHWorkflow(Files.readString(path)) match {
-              case Success(workflow) if workflow.on.vulnerableTriggers.isEmpty =>
-                logger.debug(s"$path contains no easily exploitable triggers")
-                None
-              case Success(workflow) =>
-                Option(path -> workflow)
-              case Failure(_: ScannerException) =>
-                logger.warn(s"Unable to parse '$path', it appears to be an invalid YAML file")
-                None
-              case Failure(e) if !path.getParent.equals(githubPath) =>
-                logger.debug(
-                  s"Unable to parse '$path' as a GitHub workflow file, however it's likely that it's not one",
-                  e
-                )
-                None
-              case Failure(e) =>
-                logger.warn(s"Unable to parse '$path' as a GitHub workflow file", e)
-                None
-            }
-          }
-        // Run scans on these files
-        workflowFiles.flatMap { case (path, actionsFile) =>
+        // TODO: Re-check this filtering once we have plugin vulnerability scanning done
+        // Run scans on these files, filtering out files that don't have vulnerable triggers
+        workflowFiles.filter(x => x._2.on.vulnerableTriggers.nonEmpty).flatMap { case (path, actionsFile) =>
           val relativePath = repositoryPath.relativize(path).toString
           runScans(actionsFile, scannersToRun, commit.sha, relativePath)
         }
